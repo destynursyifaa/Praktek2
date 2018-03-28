eadd44106460b099d0cfde6b808b735268a37a82
Desty NA <destynursyifaa@gmail.com>
2018-03-28 03:11:16 -0700

    Membuat Atribut dan Behavior

--- src/praktek2/Rectangle.java
+++ src/praktek2/Rectangle.java
@@ -1,4 +1,11 @@
 package praktek2;
 public class Rectangle {
+    Integer Harga_sapi;
+    Integer Jumlah_beli;
     
+    void cetakInfo(){
+        System.out.println("Harga_sapi :"+Harga_sapi);
+        System.out.println("Jumlah_beli :"+Jumlah_beli);
+        
+    }
 }
