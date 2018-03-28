28b2ee884c114fb91ac04525d9184dbafcebd8f2
Desty NA <destynursyifaa@gmail.com>
2018-03-28 03:19:40 -0700

    Mengisi Nilai Atribut

--- src/praktek2/RectangleAksi.java
+++ src/praktek2/RectangleAksi.java
@@ -3,6 +3,8 @@ package praktek2;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle rl = new Rectangle();
+        rl.Harga_sapi = 1500000;
+        rl.Jumlah_beli = 1;
         
     }
  
